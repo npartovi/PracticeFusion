5a190ce0b9a8bb4bd1eaec375d6352fb1c9df6a5
npartovi <npartovi@gmail.com>
2018-07-02 22:22:24 -0700

    created the data source, working on listing items

---
+++ src/components/doctorList.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Data from './doctors_json'
+
+class DoctorList extends React.Component{
+    constructor(props){
+        super(props)
+
+    }
+
+    renderDoctorsList(){
+
+    }
+
+    render(){
+        return(
+            <h1>This is the doctors list</h1>
+        )
+    }
+}
+
+export default DoctorList
---
+++ src/components/doctorsListItem.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class DoctorsListItem extends React.Component {
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return(
+            <h1>This is a list item</h1>
+        )
+    }
+}
+
+export default DoctorsListItem
